b11b40bf3d12b42f2f0c6b87f4ad9a5a7a38f9da
Adam Katsu <adamkatsu369@gmail.com>
2023-10-16 17:16:40 +0700

    Timer Minute and Result Array

--- public/script.js
+++ public/script.js
@@ -1,41 +1,102 @@
 let num = document.querySelector("span");
 let count = 0;
+let minutes = 0;
 let sec = 0;
 let secMinOne = 0;
 let int = null;
 
 
-// Timer Logic
+// Main Timer Logic
 
 function startTimer() {
   if (int !== null) {
+    // If Timer is Running, Stop
     clearInterval(int);
-    console.log(`timer stopped, time is = ${sec}.${secMinOne}${count}`);
+
+    // Store Result in avglist array
+    avglist.push(`${minutes}:${sec}.${secMinOne}${count}`)
+    console.log(`timer stopped, time is = ${minutes}:${sec}.${secMinOne}${count}`);
+
+    // Reset Timer 
     count = 0;
     sec = 0;
     secMinOne = 0;
     int = null;
+
   } else {
-    num.innerHTML = count;
+    // Else If Timer isn't Running, Start
     int = setInterval(addNum, 10);
     console.log(`timer started`);
   }
 }
 function addNum() {
   count += 1;
-  num.innerHTML = `${sec}.${secMinOne}${count}`;
 
-  if (count === 10) {
-    secMinOne += 1;
-    count = 0;
+  // Under 1 Minute
+  if(minutes < 1) {
+
     num.innerHTML = `${sec}.${secMinOne}${count}`;
 
-    if (secMinOne === 10) {
-      sec += 1;
-      secMinOne = 0;
+    if (count === 10) {
+      secMinOne ++;
+      count = 0;
       num.innerHTML = `${sec}.${secMinOne}${count}`;
+  
+      if (secMinOne === 10) {
+        sec ++;
+        secMinOne = 0;
+        num.innerHTML = `${sec}.${secMinOne}${count}`;
+  
+        if(sec === 60) {
+          minutes ++;
+          sec = 0;
+          num.innerHTML = `${minutes}:${sec}.${secMinOne}${count}`;
+        }
+      }
+    }
+  } else {
+
+    // Over 1 Minute
+    if(sec < 10) {
+      num.innerHTML = `${minutes}:0${sec}.${secMinOne}${count}`;
+    } else {
+      num.innerHTML = `${minutes}:${sec}.${secMinOne}${count}`;
+    }
+
+    if (count === 10) {
+      secMinOne ++;
+      count = 0;
+
+      if(sec < 10) {
+        num.innerHTML = `${minutes}:0${sec}.${secMinOne}${count}`;
+      } else {
+        num.innerHTML = `${minutes}:${sec}.${secMinOne}${count}`;
+      }
+  
+      if (secMinOne === 10) {
+        sec ++;
+        secMinOne = 0;
+
+        if(sec < 10) {
+          num.innerHTML = `${minutes}:0${sec}.${secMinOne}${count}`;
+        } else {
+          num.innerHTML = `${minutes}:${sec}.${secMinOne}${count}`;
+        }
+  
+        if(sec === 60) {
+          minutes ++;
+          sec = 0;
+          
+          if(sec < 10) {
+            num.innerHTML = `${minutes}:0${sec}.${secMinOne}${count}`;
+          } else {
+            num.innerHTML = `${minutes}:${sec}.${secMinOne}${count}`;
+          }
+        }
+      }
     }
   }
+
 }
 
 
@@ -55,3 +116,9 @@ window.addEventListener("keyup", (event) => {
 document.querySelector("button").addEventListener("click", () => {
   num.innerHTML = `0.00`;
 });
+
+
+
+// Store and Count Average
+
+let avglist = []
